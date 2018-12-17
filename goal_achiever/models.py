6d64c00d9eef9157813fcb5b2585c9dd4c2ec724
Procelius <procelius@gmail.com>
2018-12-17 23:59:15 +0200

    create Goal, Task & Restriction models, add them to admin, make migrations

--- goal_achiever/models.py
+++ goal_achiever/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Restriction(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Goal(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    tasks = models.ManyToManyField(Task)
+    restrictions = models.ManyToManyField(Restriction)
+
+    def __str__(self):
+        return self.name
